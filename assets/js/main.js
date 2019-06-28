69476e37022f9c93507cbaa87e0e711be7e66f2c
andrewjoelpeters <andrewjoelpeters@gmail.com>
2019-06-27 23:37:49 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function copyText (text) {
     // Add it to the document so that it can be focused.
     document.body.appendChild(element);
     // Focus on the element so that it can be copied.
-    element.focus();
+    element.focus({preventScroll:true});
     element.setSelectionRange(0, element.value.length);
     // Execute the copy command.
     document.execCommand('copy');
